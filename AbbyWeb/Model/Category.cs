fb018f3a3745098a8f940d3ff78de7581e0a701d
mittalmaster <mujjwalmittal@gmail.com>
2023-02-08 09:58:39 +0530

    1. Add validations in model and insert data in database with validation , both server side as well as client side validation added , Scripts is used to validate on client side , Client side means it does not hit the .cs file it has javascript code that do the things

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace AbbyWeb.Model
 {
@@ -8,6 +9,8 @@ namespace AbbyWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name="Display Order")]//this will alow to display the custom name in the property 
+        [Range(1,100,ErrorMessage ="Display Order Value must be between 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }
--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,46 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AbbyWeb.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;       
+        public Category category { get; set; }
+        //public string tname { get; set; }
+        //public string Displayorder { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                //if we add model error than modestateError is invalid by default
+                //and this will display in summary panel of cshtml file 
+                ModelState.AddModelError(string.Empty,"Name and Display order cannot be same ");
+                
+                //this will display the error below the  name feild 
+                //ModelState.AddModelError("Category.Name", "Name and Display order cannot be same");
+            }
+            if(ModelState.IsValid)// used to validate the data inside it 
+            {
+                await _db.Category.AddAsync(category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+
+            }
+            //string t = $"{tname} order is {Displayorder}";
+            //ModelState.IsValid
+            
+            return Page();
         }
     }
 }
